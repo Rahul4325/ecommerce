8e05f0b54f66f583eb930d857f2d76928016eb02
Rahul4325 <rahulkumarg7079@gmail.com>
2024-06-07 19:08:58 +0530

    change api url

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL="http://localhost:8182"
+export const API_BASE_URL="https://ecommerce-server-production-d8f1.up.railway.app/"
 const jwt=localStorage.getItem("jwt")
 
 export const api=axios.create({
